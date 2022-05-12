9c55f0e8479df46b2bdaa417b804c7fb37189eb8
Number5IsAlive <james.s.mason@hotmail.com>
2022-05-12 09:58:48 +0100

    31 Improved Targeting Selection

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -6,8 +6,15 @@ public class Targeter : MonoBehaviour
 {
     [SerializeField] private CinemachineTargetGroup cineTargetGroup = null;
 
+    private Camera maincamera;
+
     private List<Target> targets = new List<Target>();
     public Target CurrentTarget {  get; private set; }
+
+    private void Awake()
+    {
+        maincamera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,7 +33,26 @@ public class Targeter : MonoBehaviour
     {
         if (targets.Count == 0) { return false; }
 
-        CurrentTarget = targets[0];
+        Target closestTarget = null;
+        float closestTargetDistance = Mathf.Infinity;
+
+        foreach (Target target in targets)
+        {
+            Vector2 viewPos = maincamera.WorldToViewportPoint(target.transform.position);
+
+            if (viewPos.x < 0 || viewPos.x > 1 || viewPos.y < 0 || viewPos.y > 1) {  continue; }
+
+            Vector2 toCenter = viewPos - new Vector2(0.5f, 0.5f);
+            if (toCenter.SqrMagnitude() < closestTargetDistance)
+            {
+                closestTarget = target;
+                closestTargetDistance = toCenter.SqrMagnitude();
+            }
+        }
+
+        if (closestTarget == null) {  return false; }
+
+        CurrentTarget = closestTarget;
         cineTargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
         return true;
     }
